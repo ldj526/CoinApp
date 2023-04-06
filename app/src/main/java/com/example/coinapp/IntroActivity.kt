9a4454ec3a628071ab373c1138f61fbadbeb7aa8
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-04-06 23:04:11 +0900

    feat: Splash Activity 기능 구현

--- app/src/main/java/com/example/coinapp/IntroActivity.kt
+++ app/src/main/java/com/example/coinapp/IntroActivity.kt
@@ -2,14 +2,18 @@ package com.example.coinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import timber.log.Timber
 
-class MainActivity : AppCompatActivity() {
+class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_intro)
 
         Timber.d("onCreate")
+
     }
 }
