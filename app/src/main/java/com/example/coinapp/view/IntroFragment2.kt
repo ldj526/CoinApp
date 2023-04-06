d1abe25bf179280b903e4683bd14b31570a4c227
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-04-06 23:33:39 +0900

    feat: 코인을 선택하는 화면 xml 작성

--- app/src/main/java/com/example/coinapp/view/IntroFragment2.kt
+++ app/src/main/java/com/example/coinapp/view/IntroFragment2.kt
@@ -1,13 +1,19 @@
 package com.example.coinapp.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.coinapp.R
+import com.example.coinapp.databinding.FragmentIntro2Binding
 
 class IntroFragment2 : Fragment() {
+
+    private var _binding: FragmentIntro2Binding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -17,6 +23,20 @@ class IntroFragment2 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_intro2, container, false)
+        _binding = FragmentIntro2Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.nextBtn.setOnClickListener {
+            val intent = Intent(requireContext(), SelectActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
---
+++ app/src/main/java/com/example/coinapp/view/SelectActivity.kt
@@ -0,0 +1,12 @@
+package com.example.coinapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.coinapp.R
+
+class SelectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select)
+    }
+}
