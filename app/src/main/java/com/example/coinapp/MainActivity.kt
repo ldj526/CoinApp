58180448899b6996ba8b2a372cc956f3da6673c7
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-04-06 22:22:39 +0900

    feat: 기본 세팅

--- app/src/main/java/com/example/coinapp/MainActivity.kt
+++ app/src/main/java/com/example/coinapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.coinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Timber.d("onCreate")
+        Timber.tag("MainActivity").d("onCreate")
+        Timber.tag("MainActivity").e("onCreate")
     }
 }
