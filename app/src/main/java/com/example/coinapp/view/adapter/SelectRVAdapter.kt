3427502b6581e41fa8d191e5d841eccd8006d409
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-04-07 11:08:28 +0900

    feat: 이미지 클릭 시 이미지가 바뀌는 것을 RecyclerView에 매핑

--- app/src/main/java/com/example/coinapp/view/adapter/SelectRVAdapter.kt
+++ app/src/main/java/com/example/coinapp/view/adapter/SelectRVAdapter.kt
@@ -13,6 +13,9 @@ import com.example.coinapp.datamodel.CurrentPriceResult
 
 class SelectRVAdapter(val context: Context, private val coinPriceList: List<CurrentPriceResult>) :
     RecyclerView.Adapter<SelectRVAdapter.ViewHolder>() {
+
+    val selectedCoinList = ArrayList<String>()
+
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val coinName: TextView = view.findViewById(R.id.coinName)
         val coinPriceUpDown: TextView = view.findViewById(R.id.coinPriceUpDown)
@@ -40,6 +43,31 @@ class SelectRVAdapter(val context: Context, private val coinPriceList: List<Curr
         } else {
             holder.coinPriceUpDown.text = "상승입니다"
             holder.coinPriceUpDown.setTextColor(Color.parseColor("#d90429"))
+        }
+
+        val likeImage = holder.likeImage
+        // 현재 클릭된 코인
+        val currentCoin = coinPriceList[position].coinName
+
+        // RecyclerView 는 view를 그려줄 때 재활용한다.
+        // 체크 안한 항목이 체크되어 있는 것을 방지하기 위함
+        if (selectedCoinList.contains(currentCoin)) {
+            likeImage.setImageResource(R.drawable.like_red)
+        } else {
+            likeImage.setImageResource(R.drawable.like_grey)
+        }
+
+        likeImage.setOnClickListener {
+
+            if (selectedCoinList.contains(currentCoin)) {
+                selectedCoinList.remove(currentCoin)
+                // 색 변경
+                likeImage.setImageResource(R.drawable.like_grey)
+            } else {
+                selectedCoinList.add(currentCoin)
+                // 색 변경
+                likeImage.setImageResource(R.drawable.like_red)
+            }
         }
     }
 }
