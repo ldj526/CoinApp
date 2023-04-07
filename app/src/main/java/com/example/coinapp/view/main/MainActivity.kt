63c143f7f897845d2f1c78c9f84090b32375b7cb
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-04-07 22:55:52 +0900

    feat: 세팅화면 xml 디자인

--- app/src/main/java/com/example/coinapp/view/main/MainActivity.kt
+++ app/src/main/java/com/example/coinapp/view/main/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.coinapp.view.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.coinapp.R
 import com.example.coinapp.databinding.ActivityMainBinding
+import com.example.coinapp.view.setting.SettingActivity
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +20,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         Timber.d("onCreate")
+
+        binding.setting.setOnClickListener {
+            val intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
+        }
 
         // bottomNavigation 연결
         val bottomNavigationView = binding.bottomNavigationView
---
+++ app/src/main/java/com/example/coinapp/view/setting/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.coinapp.view.setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.coinapp.R
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
