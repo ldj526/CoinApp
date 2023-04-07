27ae8735e83718e3c5af064b3c632f82e51a7a8e
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-04-07 19:40:37 +0900

    feat: 관심있어 한 코인 데이터 DB에서 받아오는 기능

--- app/src/main/java/com/example/coinapp/background/GetCoinPriceRecentTradedWorkManager.kt
+++ app/src/main/java/com/example/coinapp/background/GetCoinPriceRecentTradedWorkManager.kt
@@ -3,6 +3,7 @@ package com.example.coinapp.background
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.example.coinapp.repository.DBRepository
 import timber.log.Timber
 
 // 최근 거래된 코인 가격 내역 가져오기
@@ -11,10 +12,23 @@ class GetCoinPriceRecentTradedWorkManager(
     val context: Context,
     workerParameters: WorkerParameters
 ) : CoroutineWorker(context, workerParameters) {
+
+    private val dbRepository = DBRepository()
+
     override suspend fun doWork(): Result {
         Timber.d("doWork")
 
         return Result.success()
+    }
+
+
+    // 관심있어한 코인 데이터 가져오기
+    suspend fun getAllInterestSelectedCoinData() {
+        val selectedCoinList = dbRepository.getAllInterestSelectedCoinData()
+
+        for (coinData in selectedCoinList) {
+            Timber.d(coinData.toString())
+        }
     }
 
 }
