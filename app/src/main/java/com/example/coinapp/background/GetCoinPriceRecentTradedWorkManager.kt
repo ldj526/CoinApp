f668600ba1f8892ced398eba715d07826cea91d6
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-04-07 22:08:26 +0900

    feat: 코인 가격 변동 정보 DB에 저장

--- app/src/main/java/com/example/coinapp/background/GetCoinPriceRecentTradedWorkManager.kt
+++ app/src/main/java/com/example/coinapp/background/GetCoinPriceRecentTradedWorkManager.kt
@@ -3,9 +3,12 @@ package com.example.coinapp.background
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.example.coinapp.db.entity.SelectedCoinPriceEntity
+import com.example.coinapp.network.model.RecentCoinPriceList
 import com.example.coinapp.repository.DBRepository
 import com.example.coinapp.repository.NetWorkRepository
 import timber.log.Timber
+import java.util.*
 
 // 최근 거래된 코인 가격 내역 가져오기
 // 주기적으로 로컬 데이터를 네트워크와 동기화
@@ -30,6 +33,8 @@ class GetCoinPriceRecentTradedWorkManager(
     suspend fun getAllInterestSelectedCoinData() {
         val selectedCoinList = dbRepository.getAllInterestSelectedCoinData()
 
+        val timestamp = Calendar.getInstance().time
+
         for (coinData in selectedCoinList) {
 
             Timber.d(coinData.toString())
@@ -38,7 +43,30 @@ class GetCoinPriceRecentTradedWorkManager(
             val recentCoinPriceList = netWorkRepository.getInterestCoinPriceData(coinData.coin_name)
 
             Timber.d(recentCoinPriceList.toString())
+
+            saveSelectedCoinPrice(coinData.coin_name, recentCoinPriceList, timestamp)
+
         }
+    }
+
+    // 관심있는 코인 각각의 가격 변동 정보 DB에 저장
+    fun saveSelectedCoinPrice(
+        coinName: String,
+        recentCoinPriceList: RecentCoinPriceList,
+        timestamp: Date
+    ) {
+        val selectedCoinPriceEntity = SelectedCoinPriceEntity(
+            0,
+            coinName,
+            recentCoinPriceList.data[0].transaction_date,
+            recentCoinPriceList.data[0].type,
+            recentCoinPriceList.data[0].units_traded,
+            recentCoinPriceList.data[0].price,
+            recentCoinPriceList.data[0].total,
+            timestamp
+        )
+        // db에 저장
+        dbRepository.insertCoinPriceData(selectedCoinPriceEntity)
     }
 
 }
