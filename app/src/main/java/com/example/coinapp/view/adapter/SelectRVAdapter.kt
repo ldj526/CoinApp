dae67ae0a0f18df6ee76f0246ebc9901a9884aa1
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-04-07 10:57:37 +0900

    feat: 코인이름과 코인등락 데이터들 RecyclerView에 매핑

--- app/src/main/java/com/example/coinapp/view/adapter/SelectRVAdapter.kt
+++ app/src/main/java/com/example/coinapp/view/adapter/SelectRVAdapter.kt
@@ -1,9 +1,12 @@
 package com.example.coinapp.view.adapter
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.coinapp.R
 import com.example.coinapp.datamodel.CurrentPriceResult
@@ -11,7 +14,9 @@ import com.example.coinapp.datamodel.CurrentPriceResult
 class SelectRVAdapter(val context: Context, private val coinPriceList: List<CurrentPriceResult>) :
     RecyclerView.Adapter<SelectRVAdapter.ViewHolder>() {
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
+        val coinName: TextView = view.findViewById(R.id.coinName)
+        val coinPriceUpDown: TextView = view.findViewById(R.id.coinPriceUpDown)
+        val likeImage: ImageView = view.findViewById(R.id.likeBtn)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -24,6 +29,17 @@ class SelectRVAdapter(val context: Context, private val coinPriceList: List<Curr
     override fun getItemCount(): Int = coinPriceList.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // 코인 이름 연결
+        holder.coinName.text = coinPriceList[position].coinName
+        // 코인 등락 현황 연결
+        val fluctate_24H = coinPriceList[position].coinInfo.fluctate_24H
 
+        if (fluctate_24H.contains("-")) {
+            holder.coinPriceUpDown.text = "하락입니다"
+            holder.coinPriceUpDown.setTextColor(Color.parseColor("#4361ee"))
+        } else {
+            holder.coinPriceUpDown.text = "상승입니다"
+            holder.coinPriceUpDown.setTextColor(Color.parseColor("#d90429"))
+        }
     }
 }
