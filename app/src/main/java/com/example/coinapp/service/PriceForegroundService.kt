df66cee59f8343002ff4ac049d8c5bc68a14f25f
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-04-07 23:10:35 +0900

    feat: Notification 띄우는 기능

--- app/src/main/java/com/example/coinapp/service/PriceForegroundService.kt
+++ app/src/main/java/com/example/coinapp/service/PriceForegroundService.kt
@@ -1,11 +1,18 @@
 package com.example.coinapp.service
 
-import android.app.Service
+import android.app.*
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.example.coinapp.R
+import com.example.coinapp.view.main.MainActivity
 import timber.log.Timber
 
 class PriceForegroundService : Service() {
+
+    private val NOTIFICATION_ID = 10000
 
     override fun onCreate() {
         super.onCreate()
@@ -16,6 +23,7 @@ class PriceForegroundService : Service() {
         when (intent?.action) {
             "START" -> {
                 Timber.d("START")
+                startForeground(NOTIFICATION_ID, makeNotification())
             }
             "STOP" -> {
                 Timber.d("STOP")
@@ -27,5 +35,42 @@ class PriceForegroundService : Service() {
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
+    }
+
+    // Notification 띄우기
+    fun makeNotification(): Notification {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val builder = NotificationCompat.Builder(this, "CHANNEL_ID")
+            .setSmallIcon(R.drawable.ic_baseline_access_alarms_24)
+            .setContentTitle("title")
+            .setContentText("content")
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val name = "name"
+            val descriptionText = "descriptionText"
+            val importance = NotificationManager.IMPORTANCE_LOW
+            val channel = NotificationChannel("CHANNEL_ID", name, importance).apply {
+                description = descriptionText
+            }
+
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+        return builder.build()
     }
 }
