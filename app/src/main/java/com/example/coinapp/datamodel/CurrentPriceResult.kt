165a32bf303d277cea8e76c1abdb521e3bad81da
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-04-07 09:36:38 +0900

    feat: API 데이터를 원하는 포맷으로 변경

---
+++ app/src/main/java/com/example/coinapp/datamodel/CurrentPriceResult.kt
@@ -0,0 +1,6 @@
+package com.example.coinapp.datamodel
+
+data class CurrentPriceResult(
+    val coinName: String,
+    val coinInfo: CurrentPrice
+)
--- app/src/main/java/com/example/coinapp/view/SelectViewModel.kt
+++ app/src/main/java/com/example/coinapp/view/SelectViewModel.kt
@@ -2,7 +2,10 @@ package com.example.coinapp.view
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.coinapp.datamodel.CurrentPrice
+import com.example.coinapp.datamodel.CurrentPriceResult
 import com.example.coinapp.repository.NetWorkRepository
+import com.google.gson.Gson
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
@@ -10,10 +13,29 @@ class SelectViewModel : ViewModel() {
 
     private val netWorkRepository = NetWorkRepository()
 
+    private lateinit var currentPriceResultList: ArrayList<CurrentPriceResult>
+
     // Coroutine
     fun getCurrentCoinList() = viewModelScope.launch {
         val result = netWorkRepository.getCurrentCoinList()
 
-        Timber.d(result.toString())
+        currentPriceResultList = ArrayList()
+
+        // Data Format 변환
+        for (coin in result.data) {
+            // API 에서 마지막 데이터의 포맷이 다르므로 예외처리를 해준다.
+            try {
+                val gson = Gson()
+                val gsonToJson = gson.toJson(result.data[coin.key])
+                val gsonFromJson = gson.fromJson(gsonToJson, CurrentPrice::class.java)
+
+                val currentPriceResult = CurrentPriceResult(coin.key, gsonFromJson)
+
+                currentPriceResultList.add(currentPriceResult)
+
+            } catch (e: java.lang.Exception) {
+                Timber.d(e.toString())
+            }
+        }
     }
 }
