5ec6cc7dc8a5e943d809c3a8d14c9d2f034bd25c
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-04-07 13:18:23 +0900

    feat: Flag 값을 viewModel을 통해 확인하고 이동하는 기능 구현

--- app/src/main/java/com/example/coinapp/view/intro/IntroActivity.kt
+++ app/src/main/java/com/example/coinapp/view/intro/IntroActivity.kt
@@ -1,9 +1,12 @@
 package com.example.coinapp.view.intro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Observer
+import com.example.coinapp.MainActivity
 import com.example.coinapp.R
 import timber.log.Timber
 
@@ -20,6 +23,15 @@ class IntroActivity : AppCompatActivity() {
         Timber.d("onCreate")
 
         viewModel.checkFirstFlag()
+        viewModel.first.observe(this, Observer {
+            if (it) {
+                // 처음 접속이 아닌 사용자는 MainActivity로 이동
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                // 처음 접속인 사용자
+            }
+        })
 
     }
 }
